fef6b5db2287d26cc38309dfc572d925eddc6934
thr <15242802@qq.com>
2020-10-09 16:21:50 +0800

    day 2

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"gdd"
 	"net/http"
 )
 
 func main() {
 	r := gdd.New()
-
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *gdd.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	r.Run(":80")
+	r.GET("/hello", func(c *gdd.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *gdd.Context) {
+		c.JSON(http.StatusOK, gdd.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	r.Run(":9999")
 }
