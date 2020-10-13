414bd1a9a73d082bc447d2143c6f9e6934c8b384
thr <15242802@qq.com>
2020-10-13 17:12:22 +0800

    add trie router

--- main.go
+++ main.go
@@ -8,18 +8,25 @@ import (
 func main() {
 	r := gdd.New()
 	r.GET("/", func(c *gdd.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		c.HTML(http.StatusOK, "<h1>Hello gdd</h1>")
 	})
+
 	r.GET("/hello", func(c *gdd.Context) {
-		// expect /hello?name=geektutu
+		// expect /hello?name=gddktutu
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *gdd.Context) {
-		c.JSON(http.StatusOK, gdd.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+	r.GET("/hello/:name/", func(c *gdd.Context) {
+		// expect /hello/gddktutu
+		c.String(http.StatusOK, "hello %s,you're at %s\n", c.Param("name"), c.Path)
+	})
+	r.GET("/hello/:name/:age/", func(c *gdd.Context) {
+		// expect /hello/gddktutu
+		c.String(http.StatusOK, "hello %s,%s, you're at %s\n", c.Param("name"), c.Param("age"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gdd.Context) {
+		c.JSON(http.StatusOK, gdd.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
